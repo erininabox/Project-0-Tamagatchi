c3f39cbedc652ac3deb1603ba3c3149bfbd978be
erininabox <erininabox@gmail.com>
2021-07-20 15:41:30 -0700

    added id tags to css and js files, created js constants

---
+++ js/app.js
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////
+// Global Constants by ID Tag
+const petPicArea = document.getElementById('pet-pic-area');
+const petPhoto = document.getElementById('pet-photo');
+const petName = document.getElementById('pet-name');
+
+const namePrompt = document.getElementById('name-prompt');
+const askForName = document.getElementById('ask-for-name');
+const nameForm = document.getElementById('name-form');
+const nameField = document.getElementById('name-field');
+const btnNameSubmit = document.getElementById('btn-name-submit');
+
+const actionsMenu = document.getElementById('actions-menu');
+const actionsList = document.getElementById('actions-list');
+const actionsFeed = document.getElementById('actions-feed');
+const actionsSleep = document.getElementById('actions-sleep');
+const actionsPlay = document.getElementById('actions-play');
+
+const statusArea = document.getElementById('status-area');
+const statusBars = document.getElementById('status-bars');
+const statusHungry = document.getElementById('status-hungry');
+const statusSleepy = document.getElementById('status-sleepy');
+const statusBored = document.getElementById('status-bored');
+
+const backSubmit = document.getElementById('back-submit');
+const btnBack = document.getElementById('btn-back');
+const btnSubmit = document.getElementById('btn-submit');
+
+const footerLinks = document.getElementById('footer-links');
+const linkGithubRepo = document.getElementById('link-github-repo');
+const linkGithubDemo = document.getElementById('link-github-demo');
+
+
