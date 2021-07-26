0697d709c77317a3fe02277d2bbd55ca6480b554
Erin Halden <86336422+erininabox@users.noreply.github.com>
2021-07-25 18:45:58 -0700

    trying the permalinks

--- js/app.js
+++ js/app.js
@@ -107,16 +107,16 @@ class Pet {
     };
     updateNotify() {
         if (myPet.hunger>=6) {
-            petPhoto.src='../img/hungry.jpg';
+            petPhoto.src='https://github.com/erininabox/Project-0-Tamagatchi/blob/418c52bdb0108dc8e962b18f93cb366042b13476/img/hungry.jpg';
             notifyMessage.innerText = myPet.petName + ' is hungry!'
         } else if (myPet.sleepy>=6) {
-            petPhoto.src='../img/sleepy.jpg';
+            petPhoto.src='https://github.com/erininabox/Project-0-Tamagatchi/blob/418c52bdb0108dc8e962b18f93cb366042b13476/img/sleepy.jpg';
             notifyMessage.innerText = myPet.petName + ' is sleepy!'
         } else if (myPet.bored>=6) {
-            petPhoto.src='../img/bored.jpg';
+            petPhoto.src='https://github.com/erininabox/Project-0-Tamagatchi/blob/418c52bdb0108dc8e962b18f93cb366042b13476/img/bored.jpg';
             notifyMessage.innerText = myPet.petName + ' is bored!'
         } else {
-            petPhoto.src='../img/happy.jpg';
+            petPhoto.src='https://github.com/erininabox/Project-0-Tamagatchi/blob/418c52bdb0108dc8e962b18f93cb366042b13476/img/happy.jpg';
             notifyMessage.innerText = '';
         };
     };
